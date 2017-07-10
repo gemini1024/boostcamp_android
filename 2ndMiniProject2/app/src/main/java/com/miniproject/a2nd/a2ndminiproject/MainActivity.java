09ad1e3060f8037cb672413a3abad7fd3a04439c
JiHun-Choi <gemini10244@gmail.com>
2017-07-10 20:31:58 +0900

    init

---
+++ 2ndMiniProject2/app/src/main/java/com/miniproject/a2nd/a2ndminiproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.miniproject.a2nd.a2ndminiproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
